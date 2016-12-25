4ff18e970b3d11832809958b3cd1d8a4143cec90
Siddhartha1234 <es15btech11018@iith.ac.in>
2016-12-25 21:39:29 +0530

    travis test for docs error

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ class BuildDocsCommand(setuptools.command.build_py.build_py):
     def run(self):
         errOne=call(self.apidoc_command)
         errTwo=call(self.doc_command)
-	sys.exit(errOne or errTwo)
+        sys.exit(errOne or errTwo)
 
 # Generate API documentation only if we are running on readthedocs.io
 on_rtd = getenv('READTHEDOCS', None) != None
